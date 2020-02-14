3c97278a1ee2843f34be379077d85b6cc6cb3935
shanelonergan <sptlonergan@gmail.com>
2020-02-14 16:28:19 -0500

    sign in working

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://members.actorsequity.org/signin/?returnurl=%2F');
-    await page.keyboard.type('Slonergan');
+    await page.type('#Username', 'Slonergan');
     await page.keyboard.press('Tab');
-    await page.mouse.click(720, 720);
+    await page.keyboard.type('password');
+    await page.click('#submit-signin-local')
     await page.screenshot({path: 'example.png'});
 
     await browser.close();
