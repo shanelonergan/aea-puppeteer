bbbfbfe322ffa503a2acd13f62df328d4969bdc5
shanelonergan <sptlonergan@gmail.com>
2020-02-14 16:13:36 -0500

    adding ketboard events

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://members.actorsequity.org/signin/?returnurl=%2F');
-  await page.screenshot({path: 'example.png'});
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://members.actorsequity.org/signin/?returnurl=%2F');
+    await page.keyboard.type('Slonergan');
+    await page.keyboard.press('Tab');
+    await page.mouse.click(720, 720);
+    await page.screenshot({path: 'example.png'});
 
-  await browser.close();
+    await browser.close();
 })();
