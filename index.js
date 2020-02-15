eaaab69033b39dc78c4b1b5d8e0a7369588809c0
shanelonergan <sptlonergan@gmail.com>
2020-02-15 13:11:03 -0500

    can acess number of signups

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ require('dotenv').config();
     await page.keyboard.type(process.env.PASSWORD);
     await page.click('#submit-signin-local')
     await page.goto('https://members.actorsequity.org/castingcall/signups/');
-    // const resultNum = await page.$("#lblEPAResults")
-    // console.log(resultNum.textContent)
+    const numSignups = await page.evaluate( () => {
+        return document.querySelector('#lblEPAResults').innerText
+    })
+    console.log(numSignups)
     await page.screenshot({path: 'example.png'});
 
     await browser.close();
