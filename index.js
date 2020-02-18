91eb91adebdfd33551910dc7b59c226eb444b335
shanelonergan <sptlonergan@gmail.com>
2020-02-18 12:31:47 -0500

    getting closer

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const puppeteer = require('puppeteer');
 require('dotenv').config();
 
- const signup = async () => {
+ const signup = async (auditionTitle) => {
 
     // time stuff
-    const  now = new Date();
-    const  millisTill12 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 12, 0, 0, 0) - now;
-    if (millisTill10 < 0) {
-        console.log('it is after noon!')
-    } else {
-        setTimeout(function(){alert("It's 12pm!")}, millisTill12);
-    }
+    // const  now = new Date();
+    // const  millisTill12 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 12, 0, 0, 0) - now;
+    // if (millisTill12 < 0) {
+    //     console.log('it is after noon!')
+    // } else {
+    //     setTimeout(function(){alert("It's 12pm!")}, millisTill12);
+    // }
 
-
-    const auditionTitle = 'Cape May Stage 2020 Season'
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://members.actorsequity.org/signin/?returnurl=%2F');
@@ -22,6 +20,7 @@ require('dotenv').config();
     await page.keyboard.type(process.env.PASSWORD);
     await page.click('#submit-signin-local')
     await page.goto('https://members.actorsequity.org/castingcall/signups/');
+    await page.screenshot({path: 'signups.png'});
     const numSignups = await page.evaluate( () => {
         const span = document.querySelector('#lblEPAResults')
         return span.innerText
@@ -32,9 +31,14 @@ require('dotenv').config();
         await browser.close();
     } else {
         const audition = await page.evaluate( () => {
-            const auditionCards = document.querySelectorAll('a')
-            auditionCards.filter(card => card.innerText.includes(auditionTitle))
+            const auditionColumn = document.getElementById('leftcolumnmain')
+            return auditionCards
+            // const filteredAuditions = auditionCards.filter(card => card.innerText.includes(auditionTitle))
+            // console.log(filteredAuditions)
+            // return filteredAuditions
         })
+        console.log(audition)
+        await page.click(audition)
         await page.screenshot({path: 'example.png'});
         await browser.close();
     }
